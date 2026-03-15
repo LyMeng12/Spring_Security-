bed972c408ba460ff237f6ef43596b4a35cb32f6
LyMeng12 <mengsaoly@gmail.com>
2026-03-15 12:35:49 +0700

    Security

---
+++ src/main/java/com/example/Srping_Security/SrpingSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Srping_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Srping_Security/SrpingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Srping_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
